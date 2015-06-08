5db718104de75606239cb4d27fcb8e16c5cf3afe
U-Windows8\EARNEST <yucab@uni.edu>
2015-06-07 22:50:37 -0500

    Done with starting and ending time

--- src/countGenerator.java
+++ src/countGenerator.java
@@ -1,22 +1,70 @@
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 	 
 
 public class countGenerator {
+	String time1;
+	String time2;
+	SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd hh:mm a");
 	public int getStartingTimeFromInput(int i) {
-		// TODO Auto-generated method stub
 		return 5;
 	}
 
-public int getTimeDiffereces(int Hourdiff) throws Exception{
-			String time1 = "2001.09.01 11:10 PM";
-			String time2 = "2001.09.02 12:00 AM";
-			SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd hh:mm a");
+public int getTimeDiffereces(String time1, String time2) throws Exception{
 			Date date1 = format.parse(time1);
 			Date date2 = format.parse(time2);
 			long difference = date2.getTime() - date1.getTime();
-			return Hourdiff = (int) (difference/1000/60/60);
+			return (int) (difference/1000/60/60);
+			
 		}
+public String getStartingTimeCheck(String t) {
+	Date inputDate = null;
+	try {
+		inputDate = format.parse(t);
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	String[] split = t.split(" ");
+	Date dateBound = null;
+	try {
+		dateBound = format.parse(split[0]+" 5:00 PM");
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	long difference = inputDate.getTime() - dateBound.getTime();
+    if(difference<0){
+    	return "5:00 PM";
+    }
+    else
+    	return split[1]+" PM";
+}
+
+public String getEndingTimeCheck(String endTime) {
+	Date inputDate2 = null;
+	try {
+		inputDate2 = format.parse(endTime);
+	} catch (ParseException e) {
+		e.printStackTrace();
+	}
+	String[] split = endTime.split(" ");
+	Date dateBound = null;
+	try {
+		dateBound = format.parse(split[0]+" 4:00 AM");
+	} catch (ParseException e) {
+		e.printStackTrace();
+	}
+	long difference = inputDate2.getTime() - dateBound.getTime();
+    if(difference>0){
+    	return "4:00 AM";
+    }
+    else
+    	return split[1]+" AM";
+}
+
+
 
 }
--- tests/countGeneratorTest.java
+++ tests/countGeneratorTest.java
@@ -7,11 +7,23 @@ public class countGeneratorTest {
 	@Test
 	public void startsAt5PM() throws Exception {
 		assertEquals(5, new countGenerator().getStartingTimeFromInput(5));
-	}
-
+	} 
+//I have tried Integer Hours Calculations and it works
 	@Test
 	public void TimeDifferecesByNonIntegerHours() throws Exception{
-		assertEquals(0, new countGenerator().getTimeDiffereces(0));
+		assertEquals(0, new countGenerator().getTimeDiffereces("2001.09.01 11:10 PM","2001.09.02 12:00 AM"));
 	}
+	private int getTimeDiffereces(String time1,String time2) throws Exception {
+		return new countGenerator().getTimeDiffereces(time1, time2);
+	}
+	@Test
+	public void startsNoEarlierThan5PM() throws Exception {
+		assertEquals("5:00 PM", new countGenerator().getStartingTimeCheck("2001.09.01 4:49 PM"));
+	} 
+	@Test
+	public void startsNolaterThan4AM() throws Exception {
+		assertEquals("4:00 AM", new countGenerator().getEndingTimeCheck("2001.09.02 4:15 AM"));
+	} 
+	
 }
 
